f64259b575e12a8a502a067afa5ee95bab724efc
Yash visavadiya <yash.visavadiya@tarktech.com>
2024-01-11 15:47:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise1_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
